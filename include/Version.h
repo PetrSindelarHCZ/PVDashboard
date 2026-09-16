0b11b3123f4011c1a72e69c0cfa02c5de8156d4c
PetrSindelarHCZ <83451369+PetrSindelarHCZ@users.noreply.github.com>
2026-09-16 06:50:24 +0200

    release: bump firmware to 0.1.6

--- include/Version.h
+++ include/Version.h
@@ -1,5 +1,5 @@
 #pragma once
 
 #define FIRMWARE_NAME       "Home Dashboard ESP32"
-#define FIRMWARE_VERSION    "0.1.5"
+#define FIRMWARE_VERSION    "0.1.6"
 #define FIRMWARE_BUILD_DATE "release " FIRMWARE_VERSION
