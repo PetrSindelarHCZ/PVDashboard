410c281193e09adee989de2f56e18eee70b96ac9
PetrSindelarHCZ <petr.sindelar@gmail.com>
2026-09-17 15:57:29 +0200

    Update firmware version to 1.26.260.2

--- include/Version.h
+++ include/Version.h
@@ -1,5 +1,5 @@
 #pragma once
 
 #define FIRMWARE_NAME       "Home Dashboard ESP32"
-#define FIRMWARE_VERSION    "1.26.260.1"
+#define FIRMWARE_VERSION    "1.26.260.2"
 #define FIRMWARE_BUILD_DATE "release " FIRMWARE_VERSION
