95aabac48a3a442834ade2d4da8ae2fac1198534
PetrSindelarHCZ <83451369+PetrSindelarHCZ@users.noreply.github.com>
2026-09-17 09:56:51 +0200

    Prepare first full firmware release 1.0.0

--- include/Version.h
+++ include/Version.h
@@ -1,5 +1,5 @@
 #pragma once
 
 #define FIRMWARE_NAME       "Home Dashboard ESP32"
-#define FIRMWARE_VERSION    "0.1.8"
+#define FIRMWARE_VERSION    "1.0.0"
 #define FIRMWARE_BUILD_DATE "release " FIRMWARE_VERSION
