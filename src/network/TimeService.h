58d9b0daf7b8fd734f91fd4fc7861a1068f98c3a
PetrSindelarHCZ <83451369+PetrSindelarHCZ@users.noreply.github.com>
2026-09-18 00:14:12 +0200

    Add scheduled NTP resynchronization state

--- src/network/TimeService.h
+++ src/network/TimeService.h
@@ -7,7 +7,7 @@ public:
     TimeService();
 
     void begin(const String& timezone, const String& ntpServer);
-    void loop();
+    void loop(bool wifiConnected);
     bool isSynced() const;
     String getTimeStr();    // HH:MM bez sekund
     String getDateStr();    // d. m. YYYY
@@ -15,10 +15,14 @@ public:
     static String getNtpStatusJson(bool wifiConnected);
 
 private:
+    void startNtpSync(bool preserveValidTime, const char* reason);
+
     String _timezone;
     String _ntpServer;
     bool _synced = false;
+    bool _networkAvailable = false;
     unsigned long _configuredAtMs = 0;
+    unsigned long _lastSyncAttemptMs = 0;
     unsigned long _lastSyncMs = 0;
     time_t _lastSyncEpoch = 0;
 };
