701980e9ee3029558cee4b45aae84976651dae26
PetrSindelarHCZ <83451369+PetrSindelarHCZ@users.noreply.github.com>
2026-09-18 14:01:30 +0200

    Expose weather locations as generic navigation subpages

--- src/screens/WeatherScreen.h
+++ src/screens/WeatherScreen.h
@@ -14,6 +14,14 @@ public:
     bool isSidebarEntry() const override { return _forecastDay < 0; }
     String getSidebarParentId() const override { return "weather"; }
     void buildNavigationLayout(const DataModel& dataModel, NavigationLayout& layout) const override;
+    uint8_t getNavigationSubpageCount(const DataModel& dataModel) const override {
+        return _forecastDay < 0 && dataModel.weather.locationCount > 0
+            ? dataModel.weather.locationCount
+            : 1;
+    }
+    uint8_t getInitialNavigationSubpage(const DataModel& dataModel) const override {
+        return _forecastDay < 0 ? dataModel.weather.locationIndex : 0;
+    }
 
 private:
     int8_t _forecastDay;
