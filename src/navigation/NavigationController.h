db1ef5512e9052723c186421cfd917ea99875e9a
PetrSindelarHCZ <83451369+PetrSindelarHCZ@users.noreply.github.com>
2026-09-18 14:00:58 +0200

    Extend navigation controller with generic pager callbacks

--- src/navigation/NavigationController.h
+++ src/navigation/NavigationController.h
@@ -9,10 +9,12 @@
 class NavigationController {
 public:
     using ChangeCallback = std::function<void(bool fullRefresh)>;
+    using SubpageChangeCallback = std::function<void(const String& screenId, uint8_t subpageIndex)>;
 
     NavigationController(ScreenManager& screenManager, DataModel& dataModel);
 
     void onChange(ChangeCallback callback) { _changeCallback = callback; }
+    void onSubpageChange(SubpageChangeCallback callback) { _subpageChangeCallback = callback; }
 
     // Resets focus to the sidebar entry that owns the currently active screen.
     // Used after activation from legacy WebUI endpoints or dynamic screen changes.
@@ -30,15 +32,20 @@ private:
     DataModel& _dataModel;
     NavigationState _state;
     ChangeCallback _changeCallback;
+    SubpageChangeCallback _subpageChangeCallback;
 
     uint8_t collectSidebarEntries(IScreen** entries, uint8_t maxEntries) const;
     String sidebarIdForActiveScreen() const;
     bool ensureSidebarSelection();
     void publishState();
     void notifyChange(bool fullRefresh);
+    void notifySubpageChange();
+    uint8_t activeSubpageCount() const;
+    uint8_t activeInitialSubpage() const;
 
     bool moveSidebar(int8_t delta);
-    bool enterPage(bool& screenChanged);
+    bool enterPage(bool& screenChanged, bool& subpageChanged);
+    bool movePager(NavigationAction action, bool& subpageChanged);
     bool leavePage();
     bool movePage(NavigationAction action);
     int findNeighbour(const NavigationLayout& layout, int currentIndex, NavigationAction action) const;
