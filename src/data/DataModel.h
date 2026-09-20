2442201c5e878c1f8a130ad5e52fefa0618e5ed5
PetrSindelarHCZ <83451369+PetrSindelarHCZ@users.noreply.github.com>
2026-09-20 22:47:12 +0200

    Add runtime RF sensor data model

--- src/data/DataModel.h
+++ src/data/DataModel.h
@@ -2,6 +2,7 @@
 #include <Arduino.h>
 #include "DataSourceStatus.h"
 #include "DataPoint.h"
+#include "../integrations/cc1101/RfSensorTypes.h"
 
 constexpr size_t WeatherForecastDayCount = 4;
 constexpr size_t WeatherHourlySlotsPerDay = 9;
@@ -156,6 +157,33 @@ struct PoolData {
     bool heatingActive = true;
 };
 
+
+struct RfSensorData {
+    bool configured = false;
+    bool available = false;
+    String slotId = "";
+    String name = "";
+    String protocol = "";
+    uint32_t sensorId = 0;
+    uint8_t channel = 0;
+
+    bool hasTemperature = false;
+    float temperatureC = 0.0f;
+
+    bool hasHumidity = false;
+    int humidityPercent = 0;
+
+    bool hasBattery = false;
+    bool batteryOk = true;
+
+    uint32_t lastUpdateMs = 0;
+};
+
+struct RfSensorsData {
+    uint8_t sensorCount = 0;
+    RfSensorData sensors[MaxRfSensors];
+};
+
 struct SystemData {
     String currentScreenId = "home";
 
@@ -190,6 +218,7 @@ public:
     WeatherData weather;
     InsideData inside;
     PoolData pool;
+    RfSensorsData rfSensors;
     SystemData system;
 
     void updateSystemMetrics();
