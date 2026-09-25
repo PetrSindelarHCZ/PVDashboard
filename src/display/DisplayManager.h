c66681bd8fe214bd6bbbd0cd7d0f57614a7c8737
PetrSindelarHCZ <83451369+PetrSindelarHCZ@users.noreply.github.com>
2026-09-25 13:23:20 +0200

    Restore known-good DisplayManager.h from d92f1704

--- src/display/DisplayManager.h
+++ src/display/DisplayManager.h
@@ -15,7 +15,6 @@ public:
                       const DisplayRegion* partialRegion = nullptr,
                       bool capturePreview = true);
     void requestRefresh(bool full = false);
-    void powerOff();
 
 private:
     IDisplay& _display;
